8e10d3a43cd9e59cf1c6c0c6c7e797e6b1a8d328
Madays <maday.choque@gmail.com>
2016-12-19 17:44:35 -0500

    promedio de tres numeros

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Promedio-3-numeros1
+# Promedio-3-numeros
+
+## Crear variables
+- num1
+- num2
+- num3
+- suma
+- promedio
+
+## Ingresar datos
+num1, num2, num3
+
+## Operar
+suma = num1 + num2 + num3
+promedio = suma/3
+## Mostrar
+promedio
+
+'Fin'
---
+++ main.js
@@ -0,0 +1,10 @@
+var num1, num2, suma, promedio;
+
+num1 = prompt("Por favor, ingresa el primer número");
+num2 = prompt("Por favor, ingresa el segundo número");
+num3 = prompt("Por favor, ingresa el tercer número");
+
+suma = parseFloat(num1) + parseFloat(num2) + parseFloat(num3);
+promedio = suma/3;
+
+alert(promedio);
